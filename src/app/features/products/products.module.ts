38624891cfc53eadf6132320e9d841b57a5ea084
Bassel Raafat <basselr11@gmail.com>
2026-03-12 17:57:34 +0200

    add products component

--- src/app/features/products/products.module.ts
+++ src/app/features/products/products.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductCardComponent } from './product-card/product-card.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [ProductListComponent, ProductCardComponent, ProductDetailsComponent],
   imports: [CommonModule],
 })
 export class ProductsModule {}
