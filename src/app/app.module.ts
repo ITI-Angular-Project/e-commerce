032532f1b7f4d326692501e16771cb950e485eae
Bassel Raafat <basselr11@gmail.com>
2026-03-12 17:55:09 +0200

    add layout component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { App } from './app.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { LayoutComponent } from './layout/layout/layout.component';
 
 @NgModule({
-  declarations: [App],
+  declarations: [App, NavbarComponent, FooterComponent, LayoutComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [
     provideBrowserGlobalErrorListeners(),
