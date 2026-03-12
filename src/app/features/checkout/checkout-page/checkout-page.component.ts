1169773ecf86a45ebfafab72f4dc5c7f2472e4d4
Bassel Raafat <basselr11@gmail.com>
2026-03-12 18:18:28 +0200

    add checkout component

---
+++ src/app/features/checkout/checkout-page/checkout-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checkout-page',
+  standalone: false,
+  templateUrl: './checkout-page.component.html',
+  styleUrl: './checkout-page.component.css',
+})
+export class CheckoutPageComponent {}
--- src/app/features/checkout/checkout.module.ts
+++ src/app/features/checkout/checkout.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CheckoutPageComponent } from './checkout-page/checkout-page.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [CheckoutPageComponent],
   imports: [CommonModule],
 })
 export class CheckoutModule {}
