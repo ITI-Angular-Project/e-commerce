b771ed16f4a5e42fe1ea8a02d8eac4e9db788bb2
Bassel Raafat <basselr11@gmail.com>
2026-03-14 21:38:42 +0200

    fix(routing): moving routing from app,module to app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  {
+    path: 'products',
+    loadChildren: () => import('./features/products/products.module').then((m) => m.ProductsModule),
+  },
+  {
+    path: 'cart',
+    loadChildren: () => import('./features/cart/cart.module').then((m) => m.CartModule),
+  },
+  {
+    path: 'checkout',
+    loadChildren: () => import('./features/checkout/checkout.module').then((m) => m.CheckoutModule),
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./features/auth/auth.module').then((m) => m.AuthModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,34 +10,12 @@ import { App } from './app.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { LayoutComponent } from './layout/layout.component';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [App, NavbarComponent, FooterComponent, LayoutComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot([
-      { path: '', redirectTo: 'products', pathMatch: 'full' },
-      {
-        path: 'products',
-        loadChildren: () =>
-          import('./features/products/products.module').then((m) => m.ProductsModule),
-      },
-      {
-        path: 'cart',
-        loadChildren: () => import('./features/cart/cart.module').then((m) => m.CartModule),
-      },
-      {
-        path: 'checkout',
-        loadChildren: () =>
-          import('./features/checkout/checkout.module').then((m) => m.CheckoutModule),
-      },
-      {
-        path: 'auth',
-        loadChildren: () => import('./features/auth/auth.module').then((m) => m.AuthModule),
-      },
-    ]),
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
