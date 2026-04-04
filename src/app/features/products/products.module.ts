4bfcb537611f863426b83a9c3926daa0c140eb85
Mohammed Ayman <150920389+Mohammed-Ayman93@users.noreply.github.com>
2026-04-04 13:58:16 +0200

    (fix) fixing style and hover for home page (#1)
    * feat:(navbar) added navbar & install bootstrap - bootstrap-icons
    
    * feat:(footer) added footer
    
    * feat:(navbar.html) edite navbar to be like the UI
    
    * feat:(navbar.html) edite navbar to be like the UI
    
    * feat:(footer.html) edite footer to be like the UI
    
    * feat:(home) add home page
    
    * (fix) fixing style and hover for home page

--- src/app/features/products/products.module.ts
+++ src/app/features/products/products.module.ts
@@ -3,9 +3,18 @@ import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductCardComponent } from './product-card/product-card.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: ProductListComponent
+  }
+];
 @NgModule({
-  declarations: [ProductListComponent, ProductCardComponent, ProductDetailsComponent],
-  imports: [CommonModule],
+  declarations: [ProductListComponent, ProductCardComponent, ProductDetailsComponent ],
+  imports: [CommonModule , RouterModule.forChild(routes)],
+    exports: [RouterModule]
+
 })
 export class ProductsModule {}
---
+++ src/app/home/featured-section/featured-section.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-featured-section',
+  standalone: false,
+  templateUrl: './featured-section.component.html',
+  styleUrl: './featured-section.component.css',
+})
+export class FeaturedSection {}
