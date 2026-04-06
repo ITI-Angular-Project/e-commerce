d8139f42d0a93c3f163bca9a2811b84b8c0d133b
Bassel Raafat <basselr11@gmail.com>
2026-04-06 02:23:24 +0200

    feat(cart): adding cart item component

--- src/app/features/cart/cart-item/cart-item.component.ts
+++ src/app/features/cart/cart-item/cart-item.component.ts
@@ -1,17 +1,24 @@
-import { Component, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { ICartItem } from '../../../core/Models/Cart/CartItem';
 
 @Component({
   selector: 'app-cart-item',
   standalone: false,
   templateUrl: './cart-item.component.html',
   styleUrl: './cart-item.component.css',
+  host: {
+    class: "cart-item"
+  },
 })
 export class CartItemComponent {
-  public ImageSrc = input.required();
-  public Category = input.required();
-  public Title = input.required();
-  public Subtitle = input.required();
-  public Amount = input.required();
-  public Price = input.required();
-  public OriginalPrice = input.required();
+  // public ImageSrc = input.required();
+  // public ImageAlt = input.required();
+  // public Category = input.required();
+  // public Title = input.required();
+  // public Subtitle = input.required();
+  // public Amount = input.required();
+  // public Price = input.required();
+  // public OriginalPrice = input.required();
+  public item = input.required<ICartItem>();
+  public productUpdated = output<ICartItem>();
 }
