6e847f63e0bf0b2f0be83685bbab22851cf8a770
Bassel Raafat <basselr11@gmail.com>
2026-04-06 02:25:26 +0200

    feat(cart): adding cart page component and cart module changes

--- src/app/features/cart/cart.module.ts
+++ src/app/features/cart/cart.module.ts
@@ -2,9 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartPageComponent } from './cart-page/cart-page.component';
 import { CartItemComponent } from './cart-item/cart-item.component';
+import { RouterModule } from '@angular/router';
+import { CartSummaryComponent } from './cart-summary/cart-summary.component';
+import { CouponComponent } from './coupon/coupon.component';
 
 @NgModule({
-  declarations: [CartPageComponent, CartItemComponent],
-  imports: [CommonModule],
+  declarations: [CartPageComponent, CartItemComponent, CartSummaryComponent, CouponComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: CartPageComponent,
+      },
+    ]),
+  ],
 })
 export class CartModule {}
