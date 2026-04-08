5a42ea66dee3b23a3a8260a9c47aa397547edc02
Bassel Raafat <basselr11@gmail.com>
2026-04-08 13:33:48 +0200

    feat interceptor: craeted interceptor and registered it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,20 +10,17 @@ import { App } from './app.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { LayoutComponent } from './layout/layout.component';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptorInterceptor as authInterceptor } from './core/interceptors/auth-interceptor.interceptor';
 // import { CheckoutComponent } from './features/checkout page/checkout.component';
 
-
 @NgModule({
-  declarations: [
-    App,
-    NavbarComponent,
-    FooterComponent,
-    LayoutComponent,
-  ],
+  declarations: [App, NavbarComponent, FooterComponent, LayoutComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(withInterceptors([authInterceptor])),
   ],
   bootstrap: [App],
 })
