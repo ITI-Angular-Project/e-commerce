6ce4996fe4abb8f3647160fa7951769d9595866d
salma eskander <144923849+salmaeskander@users.noreply.github.com>
2026-04-08 16:31:27 +0200

    Add login and register with email validation (#14)
    * add register page
    
    * add register and auth service
    
    * add login and model
    
    * complete login and register with email validation
    
    * complete login and register with email validation
    
    ---------
    
    Co-authored-by: Salma Eskander <salmaeskander7@gmail.com>

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../../core/services/auth.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -6,4 +10,38 @@ import { Component } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {}
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+   showPw = false;
+  constructor(private fb: FormBuilder
+    , private authService: AuthService
+    , private router: Router
+  ) {
+
+   }
+
+ ngOnInit(): void {
+  this.loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+}
+
+onSubmit(): void {
+  if (this.loginForm.valid) {
+    const { email, password } = this.loginForm.value;
+    this.authService.login(email, password).subscribe({
+      next: (res: any) => {
+        console.log('Login successful:', res);
+        this.router.navigate(['/home']);
+      },
+      error: (err: any) => {
+        console.error('Login failed:', err);
+      }
+    });
+  } else {
+    this.loginForm.markAllAsTouched();
+  }
+}
+}
+
--- src/app/features/auth/register/register.component.ts
+++ src/app/features/auth/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../core/services/auth.service';
+import { Router } from '@angular/router';
+
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,4 +10,48 @@ import { Component } from '@angular/core';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+
+  showPw  = false;
+
+
+  registerForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
+    this.registerForm = this.fb.group({
+      firstName:       ['', Validators.required],
+      lastName:        ['', Validators.required],
+      email:           ['', [Validators.required, Validators.email]],
+      password:        ['', [Validators.required, Validators.minLength(6)]],
+    agreeTerms:      [false, Validators.requiredTrue],
+      newsletter:      [true]
+    });
+  }
+
+onSubmit(): void {
+  if (this.registerForm.valid) {
+    const { firstName, lastName, email, password } = this.registerForm.value;
+    const username = `${firstName}.${lastName}`;
+
+    this.authService.checkEmailExists(email).subscribe(exists => {
+      if (exists) {
+        alert('Email already exists. Please use a different email.');
+        this.router.navigate(['/auth/login']);
+      } else {
+        this.authService.register(username, email, password).subscribe({
+          next: (response) => {
+            console.log('Registration successful:', response);
+            this.router.navigate(['/auth/login']);
+          },
+          error: (error) => {
+            console.error('Registration failed:', error);
+          }
+        });
+      }
+    });
+
+  } else {
+    console.log('Form is invalid');
+  }
+}
+}
