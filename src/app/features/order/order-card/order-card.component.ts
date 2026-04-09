eb97180bc0f9598d85a9189b21a8169515682a44
Bassel Raafat <basselr11@gmail.com>
2026-04-09 20:54:37 +0200

    feat(order): adding orer card and filters

---
+++ src/app/features/order/order-card/order-card.component.ts
@@ -0,0 +1,25 @@
+import { Component, input, Input } from '@angular/core';
+import { Order } from '../../../core/Models/Order/order-item.model';
+
+@Component({
+  selector: 'app-order-card',
+  standalone: false,
+  templateUrl: './order-card.component.html',
+  styleUrl: './order-card.component.css',
+})
+export class OrderCardComponent {
+  order = input.required<Order>();
+
+  getStatusBgColor(status: string): string {
+    switch (status.toLowerCase()) {
+      case 'delivered':
+        return 'bg-success bg-opacity-10 text-success';
+      case 'shipped':
+        return 'bg-primary bg-opacity-10 text-primary';
+      case 'processing':
+        return 'bg-warning text-dark'; // Bootstrap warning is already yellow
+      default:
+        return 'bg-secondary bg-opacity-10 text-secondary';
+    }
+  }
+}
