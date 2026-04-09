2c508f8c116a77aff97a0a10d66a24f483d925f0
Bassel Raafat <basselr11@gmail.com>
2026-04-10 00:35:06 +0200

    fix

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -11,24 +11,26 @@ import { AuthService } from './../../core/services/auth.service';
 export class NavbarComponent implements OnInit {
   private cartService = inject(CartService);
   public AuthService = inject(AuthService);
-  userName = localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")!).username : "";
+  userName = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')!).username : '';
   loggedIn = false;
   show = false;
-  cartCount = this.cartService.cartItemCount;
+  cartCount = 0;
 
   ngOnInit(): void {
     this.cartService.getCart().subscribe({
+      next: () => {
+        this.cartCount = this.cartService.cartItemCount();
+      },
       error: (err) => {
         console.error('Failed to load cart count:', err);
       },
     });
   }
 
-  toglle(){
+  toglle() {
     this.show = !this.show;
-    
   }
-  logout(){
+  logout() {
     this.AuthService.logOut();
   }
 }
